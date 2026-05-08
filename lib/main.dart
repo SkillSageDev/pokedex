8b981659fdd262524ea9bd0b26d96a8e364e6fdc
SkillSageDev <147807207+SkillSageDev@users.noreply.github.com>
2026-05-08 20:55:34 +0300

    chore: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Pokedex());
+}
+
+class Pokedex extends StatelessWidget {
+  const Pokedex({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(child: Text("Hello World")),
+      ),
+    );
+  }
+}
