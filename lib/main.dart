b98c58cd38d7bbacd1946b134d085855577acf99
SkillSageDev <147807207+SkillSageDev@users.noreply.github.com>
2026-05-10 07:50:38 +0300

    chore: initialize firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pokedex/firebase_options.dart';
 import 'navigation/AppRoutes.dart';
 import 'navigation/RouterGenerator.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Pokedex());
 }
 
-class Pokedex extends StatelessWidget
-{
+class Pokedex extends StatelessWidget {
   const Pokedex({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.signup,
@@ -20,3 +22,4 @@ class Pokedex extends StatelessWidget
     );
   }
 }
+
